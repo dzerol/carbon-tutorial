57b758c9f5168ad8d6543b32462c519636d09f90
zero D <zerod@zerodeMacBook-Pro.local>
2021-03-29 18:14:32 +1100

    feat(tutorial): complete step 3

--- src/content/RepoPage/RepoTable.js
+++ src/content/RepoPage/RepoTable.js
@@ -13,7 +13,15 @@ import {
   TableExpandedRow,
 } from 'carbon-components-react';
 
+// apollo
+import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
+
 const RepoTable = ({ rows, headers }) => {
+  const getRowDescription = rowId => {
+    const row = rows.find(({ id }) => id === rowId);
+    return row ? row.description : '';
+  };
   return (
     <DataTable
       rows={rows}
@@ -48,7 +56,7 @@ const RepoTable = ({ rows, headers }) => {
                     ))}
                   </TableExpandRow>
                   <TableExpandedRow colSpan={headers.length + 1}>
-                    <p>Row description</p>
+                    <p>{getRowDescription(row.id)}</p>
                   </TableExpandedRow>
                 </React.Fragment>
               ))}
